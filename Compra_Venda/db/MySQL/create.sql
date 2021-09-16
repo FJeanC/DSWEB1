d99e058b100a705cb221da4034632012107259e6
FJeanC <jeanfagiani1@gmail.com>
2021-09-16 16:49:37 -0300

    test with images

--- Compra_Venda/db/MySQL/create.sql
+++ Compra_Venda/db/MySQL/create.sql
@@ -4,7 +4,7 @@ create database Login;
 
 use Login
 
-create table Usuario(id bigint not null auto_increment, nome varchar(256) not null, login varchar(20) not null unique, senha varchar(64) not null, papel varchar(10), primary key (id));
+create table Usuario(id int not null auto_increment, nome varchar(256) not null, login varchar(20) not null unique, senha varchar(64) not null, papel varchar(10), primary key (id));
 
 insert into Usuario(nome, login, senha, papel) values ('Administrador', 'admin', 'admin', 'ADMIN');
 
